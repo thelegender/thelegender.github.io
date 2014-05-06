af93db5d2959df9f0faf8afe339b42d44a5865b6
Jon Love <jltarheel47@gmail.com>
2014-05-05 23:40:45 -0400

    look n see

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-	alert("Hi");
+	$('#window').hide();
 	$('#welcome').mouseenter(function(){
 		$('#welcome').fadeTo('fast', 0.5);
 	});
 	$('#welcome').mouseleave(function(){
 		$('#welcome').fadeTo('fast', 1);
+	});
+	$('.button').mouseenter(function(){
+		$('.button').fadeTo('fast', 0.7);
+	});
+	$('.button').mouseleave(function(){
+		$('.button').fadeTo('fast', 1);
+	});
+	$('#close').click(function(){
+		$('#window').fadeOut(300);
+	});
+	$('.button').click(function(){
+		$('#window').fadeIn(300);
 	});
 });
