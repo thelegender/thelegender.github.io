9be0fa8b1a1f0defd98c3af6b6b9c260bfafcac2
Jon Love <jltarheel47@gmail.com>
2014-05-06 11:45:18 -0400

    random thing working

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 	$('#window').hide();
-	$('.button').mouseenter(function(){
-		$('.button').fadeTo('fast', 0.7);
-	});
-	$('.button').mouseleave(function(){
-		$('.button').fadeTo('fast', 1);
-	});
+
 	$('#close').click(function(){
 		$('#window').slideUp(400);
 	});
 	$('.button').click(function(){
-		$('#window').show(400);
+		var random = Math.floor((Math.random() * 5) + 1);
+		if (random == 1){window.location.href = "http://www.youtube.com/";}
+		else if (random == 2) {window.location.href = "http://www.roblox.com/";}
+		else if (random == 3) {window.location.href = "http://www.codecademy.com/";}
+		else if (random == 4) {window.location.href = "http://www.google.com/";}
+		else if (random == 5) {window.location.href = "http://www.github.com/";}
+
 	});
 
 
